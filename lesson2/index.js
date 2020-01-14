d4f8fc9e38a8c1f82c2d334d1bd3edce4cae2c64
‘Yonatan <yonatan@yaltman.com>
2020-01-14 23:33:04 +0200

    good night

---
+++ lesson2/index.js
@@ -0,0 +1,4 @@
+$('p').css("background-color", "red");
+$('.blue').css("background-color", "blue");
+$('#input').css("border-radius", "10px 0 10px 0");
+$("input[type='password']").css("background-color", "black").css("color", "green");
