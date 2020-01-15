0e832805c1c6f5934e11a1e955a45969e286e294
‘Yonatan <yonatan@yaltman.com>
2020-01-15 10:04:17 +0200

    lets query!!!!

--- lesson2/index.js
+++ lesson2/index.js
@@ -1,9 +1,26 @@
+// console.log('Script Ready',new Date().getTime());
 
 
-$(document).ready(function(){
-    $("button").click(function(){
-        $("ol").append("<li>list item</li>"); 
-    });
+
+$(document).ready(function () {
+    // console.log('Dom Ready',new Date().getTime());
+
+    $('button').click(someFunction);
+    document.getElementsByTagName('button')[0]
+        .addEventListener("click", (e) => {
+            console.log('addEventListener', e);
+
+        })
 });
 
 
+
+function someFunction(e) {
+    $('.h').toggle(4000).toggle(200);
+    console.log('someFunction', e);
+}
+function someFunction1() {
+    $('body')
+        .append("<h1>😎someFunction!!</h1>")
+
+}
